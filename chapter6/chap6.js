3cb54fcd9b284ec8d691ffed639018095674ac5c
saadatalli <s.saadatali9@gmail.com>
2024-02-25 23:19:24 +0500

    chapter 6

---
+++ chapter6/chap6.js
@@ -0,0 +1,63 @@
+console.log("increment and decrement exercise")
+
+var count=8;
+count++
+console.log(count)
+
+var x=15;
+--x
+console.log(x)
+
+var num=25;
+x--
+console.log(num)
+
+
+var value=10;
+var newvalue=++value
+console.log(newvalue)
+
+var index=5;
+x++
+console.log(index)
+
+var quantity=12;
+--quantity
+console.log(quantity)
+
+var counter=18;
+counter++
+console.log(counter)
+
+var score=30;
+score--
+console.log(score)
+
+
+var value=20;
+var newvalue=++value
+console.log(newvalue)
+
+var counter=8;
+counter--
+console.log(counter)
+
+document.write("concatenation exercise <br>")
+
+var firstname="john"
+document.write(firstname+"Doe <br>")
+
+var city="new"
+document.write(city+"york <br>")
+
+var fruit="apple"
+document.write(fruit+"pie<br>")
+
+document.write("happy "+"birthday "+"to you"+"<br>")
+
+document.write("open "+"source "+"intelligence"+"<br>")
+
+document.write("animal "+"black "+"bird"+"<br>")
+
+document.write("web "+"design "+"development"+"<br>")
+
