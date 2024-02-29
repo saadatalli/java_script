d8c916fc902d43af02dd26cb5f9bec8b1824c9ae
saadatalli <s.saadatali9@gmail.com>
2024-02-29 16:51:19 +0500

    chapter 5

--- chapter3/chap3.js
+++ chapter3/chap3.js
@@ -1,2 +1,12 @@
 var age = 20
 alert('I am '+age+ ' years old')
+
+var birthyear=2003
+document.write("my birth year is"+ birthyear);
+document.write("<br>");
+
+
+var vname="john joe";
+var product=" Shirts";
+var quantity=5;
+document.write(vname+"ordered"+ quantity+ product);
---
+++ chapter5/chap5.js
@@ -0,0 +1,21 @@
+var a=3;
+var b=5;
+var sum=a+b;
+var sub=b-a;
+var multi=a*b;
+var div=b/a;
+
+document.write("sum of 3 and 5 is "+sum)
+document.write("<br>");
+
+document.write("subtraction of 3 and 5 is "+sub);
+document.write("<br>");
+
+document.write("multiplication of 3 and 5 is "+multi);
+document.write("<br>");
+
+document.write("division of 3 and 5 is "+div);
+document.write("<br>");
+
+
+
